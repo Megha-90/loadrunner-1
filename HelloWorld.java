393cecb2b27fb0f358d3ac14173931556bfa44ea
Megha-90 <119483417+Megha-90@users.noreply.github.com>
2023-01-18 15:22:21 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,8 +5,8 @@ class HelloWorld {
     {
       token = args[0];  
       System.out.println("Hello, World! "+ token); 
-      HelloWorld test = new HelloWorld(token);
-      test.printing();
+      HelloWorld test = new HelloWorld();
+      test.printing(token);
     }
     public String printing(String token)
     {
