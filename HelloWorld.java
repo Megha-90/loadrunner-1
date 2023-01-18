fe7455a65d9172a11611672dce938984ad093508
Megha-90 <119483417+Megha-90@users.noreply.github.com>
2023-01-18 13:01:59 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+      TOKEN = arg[0]
+        System.out.println("Hello, World!"+TOKEN); 
+      HelloWorld test = new HelloWorld();
+      
+      test.init();
+      
+      public int init(){
+        System.out.println("Hello, World! New "+TOKEN);
+      
+    }
+}
