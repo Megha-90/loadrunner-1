24daecba07bdf4351e2fe7872eea329f337230ee
Megha-90 <119483417+Megha-90@users.noreply.github.com>
2023-01-18 15:23:35 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,14 @@
 class HelloWorld {
     static String token = "abbbsgsf";
     
-    public static void main(String args[]) 
+    public static void main(String[] args) 
     {
       token = args[0];  
       System.out.println("Hello, World! "+ token); 
       HelloWorld test = new HelloWorld();
       test.printing(token);
     }
-    public String printing(String token)
+    public String printing(String[] token)
     {
         System.out.println("Hello, World! New "+ token);
     }
