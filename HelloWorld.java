2ac7422a9d083408e1a720f072ca6ed4301bdfdd
Megha-90 <119483417+Megha-90@users.noreply.github.com>
2023-01-18 14:32:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 // Your First Program
 
 class HelloWorld {
-    static String TOKEN = arg[0];
+    //String TOKEN = arg[0];
     public static void main(String args[]) {
-      //String TOKEN = arg[0];  
+      String TOKEN = arg[0];  
       System.out.println("Hello, World!"+TOKEN); 
       HelloWorld test = new HelloWorld();
       
